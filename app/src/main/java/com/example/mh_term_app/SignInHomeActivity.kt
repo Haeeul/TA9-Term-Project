900a434b3569647e7f4f4a417f3887e9e1cb2224
Haeeul <etoilune00@gmail.com>
2023-02-03 11:18:44 +0900

    [feat] #1 로그인 홈 레이아웃

---
+++ app/src/main/java/com/example/mh_term_app/SignInHomeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mh_term_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_home)
+    }
+}
