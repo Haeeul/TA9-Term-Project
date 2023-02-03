42408dbc8860862b86df308c06216d86a8928a7a
Haeeul <etoilune00@gmail.com>
2023-02-03 14:28:46 +0900

    [feat] #1 로그인 레이아웃 추가

---
+++ app/src/main/java/com/example/mh_term_app/ui/sign/SignInHomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mh_term_app.ui.sign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mh_term_app.R
+
+class SignInHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_home)
+    }
+}
--- app/src/main/java/com/example/mh_term_app/ui/sign/in/SignInActivity.kt
+++ app/src/main/java/com/example/mh_term_app/ui/sign/in/SignInActivity.kt
@@ -1,11 +1,12 @@
-package com.example.mh_term_app
+package com.example.mh_term_app.ui.sign.`in`
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mh_term_app.R
 
-class SignInHomeActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in_home)
+        setContentView(R.layout.activity_sign_in)
     }
 }
