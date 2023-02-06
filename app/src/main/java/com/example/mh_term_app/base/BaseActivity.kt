5f2c27e5ecf44a617635ccbc9328222cba48d83c
Haeeul <etoilune00@gmail.com>
2023-02-06 13:53:42 +0900

    [feat] #4 base 파일 추가

---
+++ app/src/main/java/com/example/mh_term_app/base/BaseActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mh_term_app.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<VB : ViewDataBinding> : AppCompatActivity() {
+    lateinit var viewDataBinding: VB
+
+    abstract val layoutResID: Int
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewDataBinding = DataBindingUtil.setContentView(this, layoutResID)
+        viewDataBinding.lifecycleOwner = this@BaseActivity
+    }
+}
