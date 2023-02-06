65efaa4059d15ed46fdc438dba856d32ec99a3d6
Haeeul <etoilune00@gmail.com>
2023-02-06 09:54:48 +0900

    [feat] #1 로그인, 회원가입 인텐트 연결

---
+++ app/src/main/java/com/example/mh_term_app/util/ActivityExtension.kt
@@ -0,0 +1,13 @@
+package com.example.mh_term_app.util
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+
+fun AppCompatActivity.clearStartActivity(activity: Class<*>) {
+    startActivity(Intent(this, activity).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+}
+
+fun AppCompatActivity.startActivityWithFinish(activity: Class<*>) {
+    startActivity(Intent(this, activity).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+    this.finishAffinity()
+}
