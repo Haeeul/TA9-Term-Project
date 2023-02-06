e06b16c8fbbeebcbb0f0d2c1f2eb4e96e25779ec
Haeeul <etoilune00@gmail.com>
2023-02-06 14:03:19 +0900

    [feat] #4 BaseActivity 적용

--- app/src/main/java/com/example/mh_term_app/ui/sign/in/SignInActivity.kt
+++ app/src/main/java/com/example/mh_term_app/ui/sign/in/SignInActivity.kt
@@ -1,12 +1,21 @@
 package com.example.mh_term_app.ui.sign.`in`
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.content.Intent
+import android.view.View
 import com.example.mh_term_app.R
+import com.example.mh_term_app.base.BaseActivity
+import com.example.mh_term_app.databinding.ActivitySignInBinding
+import com.example.mh_term_app.ui.sign.up.SignUpActivity
 
-class SignInActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+class SignInActivity : BaseActivity<ActivitySignInBinding>() {
+    override val layoutResID: Int = R.layout.activity_sign_in
+
+    fun goToBackListener(view : View){
+        finish()
+    }
+
+    fun goToSignUpListener(view : View){
+        val goToSignUpIntent = Intent(this, SignUpActivity::class.java)
+        startActivity(goToSignUpIntent)
     }
 }
