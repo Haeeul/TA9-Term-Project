29dfca6c7c052e8f8e2a526338459aabbdc30cab
Haeeul <etoilune00@gmail.com>
2023-02-06 10:56:12 +0900

    [feat] #3 뒤로가기, 회원가입 버튼 인텐트 연결

--- app/src/main/java/com/example/mh_term_app/ui/sign/in/SignInActivity.kt
+++ app/src/main/java/com/example/mh_term_app/ui/sign/in/SignInActivity.kt
@@ -1,12 +1,31 @@
 package com.example.mh_term_app.ui.sign.`in`
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.mh_term_app.R
+import com.example.mh_term_app.databinding.ActivitySignInBinding
+import com.example.mh_term_app.ui.sign.up.SignUpActivity
 
 class SignInActivity : AppCompatActivity() {
+    lateinit var binding : ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
+        binding.lifecycleOwner = this
+
+        binding.edtSignInPhone.requestFocus()
+    }
+
+    fun goToBackListener(view : View){
+        finish()
+    }
+
+    fun goToSignUpListener(view : View){
+        val goToSignUpIntent = Intent(this, SignUpActivity::class.java)
+        startActivity(goToSignUpIntent)
     }
 }
