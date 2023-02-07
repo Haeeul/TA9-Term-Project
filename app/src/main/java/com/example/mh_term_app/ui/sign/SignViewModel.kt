b6c65742011970a935d523b15b675e45a042eb27
Haeeul <etoilune00@gmail.com>
2023-02-07 10:44:56 +0900

    [feat] #3 유효 시간 만료 표시 기능 추가
    타이머, 인증 재요청, 안내 문구 표시 추가

--- app/src/main/java/com/example/mh_term_app/ui/sign/SignViewModel.kt
+++ app/src/main/java/com/example/mh_term_app/ui/sign/SignViewModel.kt
@@ -5,15 +5,35 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import android.os.Handler
+import java.util.*
+import kotlin.concurrent.timer
 
 class SignViewModel(): ViewModel() {
     // 사용자 입력 내용
     val phoneNumTxt = MutableLiveData<String>("")
 
+    // 안내 문구
+    val authNotice = MutableLiveData<String>()
+    val timer = MutableLiveData<String>()
+
+    var minute = 3
+    var second = 0
+    private var timerTask: Timer? = null
+
     // 중복 확인 enable
     private val _isValidRequestBtn = MutableLiveData<Boolean>(false)
     val isValidRequestBtn : LiveData<Boolean>
         get() = _isValidRequestBtn
+
+    // 타이머 표시
+    private val _isValidTimer = MutableLiveData<Boolean>(false)
+    val isValidTimer : LiveData<Boolean>
+        get() = _isValidTimer
+
+    // 인증 유효 검사
+    private val _isValidAuth = MutableLiveData<Boolean>(false)
+    val isValidAuth : LiveData<Boolean>
+        get() = _isValidAuth
 
     // 휴대폰 번호 자릿수 확인
     fun inputPhoneNum(s: CharSequence?, start: Int, before: Int, count: Int){
@@ -21,6 +41,51 @@ class SignViewModel(): ViewModel() {
     }
 
     private fun checkPhoneLength(){
-        _isValidRequestBtn.value = phoneNumTxt.value?.length == 11
+        if(phoneNumTxt.value?.length == 11) {
+            _isValidRequestBtn.value = true
+        }else{
+            _isValidRequestBtn.value = false
+            stopAuthTimer()
+        }
+    }
+
+    private fun setTimeVisibility(visibility : Boolean){
+        if(visibility){
+            _isValidTimer.value = true
+        }else{
+            _isValidTimer.value = false
+            _isValidAuth.value = false
+        }
+    }
+
+    fun startAuthTimer(){
+        setTimeVisibility(true)
+
+        timerTask = timer(period = 1000){
+
+            if(second == 0){
+                minute--
+                second = 60;
+            }
+
+            second--
+
+            timer.postValue(String.format("%02d:%02d",minute,second))
+
+            if(minute==0&&second==0) {
+                this.cancel()
+                authNotice.postValue("인증 번호가 만료되었습니다.")
+                _isValidAuth.postValue(true)
+            }
+        }
+    }
+
+    private fun stopAuthTimer(){
+        setTimeVisibility(false)
+
+        minute = 3
+        second = 0
+
+        timerTask?.cancel()
     }
 }
--- app/src/main/java/com/example/mh_term_app/ui/sign/in/SignInActivity.kt
+++ app/src/main/java/com/example/mh_term_app/ui/sign/in/SignInActivity.kt
@@ -9,6 +9,8 @@ import com.example.mh_term_app.base.BaseActivity
 import com.example.mh_term_app.databinding.ActivitySignInBinding
 import com.example.mh_term_app.ui.sign.SignViewModel
 import com.example.mh_term_app.ui.sign.up.SignUpActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignInActivity : BaseActivity<ActivitySignInBinding>() {
     override val layoutResID: Int = R.layout.activity_sign_in
@@ -16,9 +18,15 @@ class SignInActivity : BaseActivity<ActivitySignInBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewDataBinding.vm = signInViewModel
+        viewDataBinding.apply {
+            vm = signInViewModel
+            edtSignInPhone.requestFocus()
+        }
+    }
 
-        viewDataBinding.edtSignInPhone.requestFocus()
+    fun postAuthRequest(view: View){
+        signInViewModel.startAuthTimer()
+        viewDataBinding.edtSighInAuthNum.requestFocus()
     }
 
     fun goToBackListener(view : View){
