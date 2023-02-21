39633cd6adb314e3abe8798d04d8993512d1606c
Haeeul <etoilune00@gmail.com>
2023-02-21 18:00:31 +0900

    [feat] #17 신규 제보하기(상세정보) 레이아웃 생성

---
+++ app/src/main/java/com/example/mh_term_app/ui/menu/report/store/ReportInfoStoreActivity.kt
@@ -0,0 +1,23 @@
+package com.example.mh_term_app.ui.menu.report.store
+
+import com.example.mh_term_app.MHApplication
+import com.example.mh_term_app.R
+import com.example.mh_term_app.base.BaseActivity
+import com.example.mh_term_app.databinding.ActivityReportInfoStoreBinding
+import com.example.mh_term_app.utils.extension.setSingleOnClickListener
+
+class ReportInfoStoreActivity : BaseActivity<ActivityReportInfoStoreBinding>() {
+    override val layoutResID: Int
+        get() = R.layout.activity_report_info_store
+
+    override fun initView() {
+        super.initView()
+
+        binding.tbReportInfoStore.apply {
+            title = MHApplication.getApplicationContext().getString(R.string.title_menu_report)
+            btnTbBack.setSingleOnClickListener {
+                finish()
+            }
+        }
+    }
+}
