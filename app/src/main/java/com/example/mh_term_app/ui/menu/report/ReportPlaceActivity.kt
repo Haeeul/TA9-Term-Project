8cf962d92c986510f6603ac45c3819b961f2040c
Haeeul <etoilune00@gmail.com>
2023-02-25 17:52:05 +0900

    [feat] #17 제보하기 뷰에 있는 주소 입력 지도에 카메라 리스너 수정

--- app/src/main/java/com/example/mh_term_app/ui/menu/report/ReportPlaceActivity.kt
+++ app/src/main/java/com/example/mh_term_app/ui/menu/report/ReportPlaceActivity.kt
@@ -19,10 +19,10 @@ import com.example.mh_term_app.databinding.ActivityReportPlaceBinding
 import com.example.mh_term_app.utils.extension.changeKeywordColor
 import com.example.mh_term_app.utils.extension.setSingleOnClickListener
 import com.example.mh_term_app.utils.extension.toast
-import com.naver.maps.map.LocationTrackingMode
-import com.naver.maps.map.MapFragment
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
 import com.naver.maps.map.util.FusedLocationSource
 import java.io.IOException
 import java.util.*
@@ -31,10 +31,10 @@ import java.util.*
 class ReportPlaceActivity : BaseActivity<ActivityReportPlaceBinding>(), OnMapReadyCallback {
     override val layoutResID: Int
         get() = R.layout.activity_report_place
-    private val reportPlaceViewModel : ReportViewModel by viewModels()
+    private val reportPlaceViewModel: ReportViewModel by viewModels()
 
     private lateinit var locationSource: FusedLocationSource
-    private lateinit var mapFragment : MapFragment
+    private lateinit var mapFragment: MapFragment
     private lateinit var naverMap: NaverMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,23 +45,29 @@ class ReportPlaceActivity : BaseActivity<ActivityReportPlaceBinding>(), OnMapRea
             txtReportPlaceTitle.changeKeywordColor(
                 MHApplication.getApplicationContext().getString(R.string.desc_report_place_start),
                 MHApplication.getApplicationContext().getString(R.string.desc_report_place_end),
-                10, 12, 2,4)
+                10, 12, 2, 4
+            )
         }
 
         locationSource =
             FusedLocationSource(this, MainActivity.LOCATION_PERMISSION_REQUEST_CODE)
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>,
-                                            grantResults: IntArray) {
-        if (locationSource.onRequestPermissionsResult(requestCode, permissions,
-                grantResults)) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions,
+                grantResults
+            )
+        ) {
             if (!locationSource.isActivated) { // 권한 거부됨
                 toast("위치 권한을 허용해주세요")
                 naverMap.locationTrackingMode = LocationTrackingMode.None
                 naverMap.locationOverlay.isVisible = false
-            }else{
+            } else {
                 naverMap.locationTrackingMode = LocationTrackingMode.Follow
                 naverMap.locationOverlay.isVisible = true
             }
@@ -99,18 +105,34 @@ class ReportPlaceActivity : BaseActivity<ActivityReportPlaceBinding>(), OnMapRea
         val uiSettings = naverMap.uiSettings
         uiSettings.isLocationButtonEnabled = true
 
-        naverMap.addOnCameraIdleListener  {
-//            toast(naverMap.cameraPosition.toString())
-            Log.d("명1",naverMap.cameraPosition.toString())
+        val cameraPosition = CameraPosition(
+            LatLng(
+                naverMap.cameraPosition.target.latitude,
+                naverMap.cameraPosition.target.longitude
+            ), 18.0
+        )
+        naverMap.cameraPosition = cameraPosition
 
-            Log.d("명2",getAddress(naverMap.cameraPosition.target.latitude, naverMap.cameraPosition.target.longitude))
-            binding.edtReportPlaceAddress.setText(getAddress(naverMap.cameraPosition.target.latitude, naverMap.cameraPosition.target.longitude))
+        naverMap.addOnCameraIdleListener {
+            binding.edtReportPlaceAddress.setText(
+                getAddress(
+                    naverMap.cameraPosition.target.latitude,
+                    naverMap.cameraPosition.target.longitude
+                )
+            )
         }
     }
 
-    private fun checkPermission(){
-        if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-            && ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+            && ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        )
             naverMap.locationTrackingMode = LocationTrackingMode.Follow
         else
             naverMap.locationTrackingMode = LocationTrackingMode.None
@@ -121,17 +143,12 @@ class ReportPlaceActivity : BaseActivity<ActivityReportPlaceBinding>(), OnMapRea
         val address: ArrayList<Address>
         var addressResult = "주소를 가져 올 수 없습니다."
         try {
-            //세번째 파라미터는 좌표에 대해 주소를 리턴 받는 갯수로
-            //한좌표에 대해 두개이상의 이름이 존재할수있기에 주소배열을 리턴받기 위해 최대갯수 설정
             address = geoCoder.getFromLocation(lat, lng, 1) as ArrayList<Address>
             if (address.size > 0) {
-                // 주소 받아오기
                 val currentLocationAddress = address[0].getAddressLine(0)
                     .toString()
                 addressResult = currentLocationAddress
-
             }
-
         } catch (e: IOException) {
             e.printStackTrace()
         }
@@ -145,8 +162,8 @@ class ReportPlaceActivity : BaseActivity<ActivityReportPlaceBinding>(), OnMapRea
         setPlaceObserver(reportPlaceViewModel.typeTxt)
     }
 
-    private fun setPlaceObserver(data : LiveData<String>){
-        data.observe(this){
+    private fun setPlaceObserver(data: LiveData<String>) {
+        data.observe(this) {
             reportPlaceViewModel.checkValidNextBtn()
         }
     }
@@ -166,8 +183,8 @@ class ReportPlaceActivity : BaseActivity<ActivityReportPlaceBinding>(), OnMapRea
         }
     }
 
-    fun goToReportType(view : View){
-        val className : Class<*> = if(binding.rbReportTypeStore.isChecked) {
+    fun goToReportType(view: View) {
+        val className: Class<*> = if (binding.rbReportTypeStore.isChecked) {
             ReportInfoStoreActivity::class.java
         } else {
             ReportInfoFacilityActivity::class.java
