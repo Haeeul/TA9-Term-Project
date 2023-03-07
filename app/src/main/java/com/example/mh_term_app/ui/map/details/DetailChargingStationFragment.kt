b1af0adb5176d701f68b3dea90c0591926908d91
Haeeul <etoilune00@gmail.com>
2023-03-08 06:16:41 +0900

    [feat] #13 충전소 상세 정보 레이아웃 생성

---
+++ app/src/main/java/com/example/mh_term_app/ui/map/details/DetailChargingStationFragment.kt
@@ -0,0 +1,11 @@
+package com.example.mh_term_app.ui.map.details
+
+import com.example.mh_term_app.R
+import com.example.mh_term_app.base.BaseFragment
+import com.example.mh_term_app.databinding.FragmentDetailChargingStationBinding
+
+class DetailChargingStationFragment : BaseFragment<FragmentDetailChargingStationBinding>() {
+    override val layoutResID: Int
+        get() = R.layout.fragment_detail_charging_station
+
+}
